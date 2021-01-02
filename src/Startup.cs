c6e4ee6f74295196635e556630aa388448f665bc
Ivan Ballesteros <ivan.ballesteros@csgi.com>
2021-01-02 15:07:32 -0500

    Versioning with conventions

--- src/Startup.cs
+++ src/Startup.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using AutoMapper;
+using CoreCodeCamp.Controllers;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -34,6 +36,14 @@ namespace CoreCodeCamp
                 opt.ApiVersionReader = ApiVersionReader.Combine(
                     new HeaderApiVersionReader("X-Version"),
                     new QueryStringApiVersionReader("ver", "version"));
+
+                opt.Conventions.Controller<TalksController>()
+                    .HasApiVersion(new ApiVersion(1, 0))
+                    .HasApiVersion(new ApiVersion(1, 1))
+                    .Action(c => c.Delete(default(string), default(int)))
+                        .MaoToApiVersion(1, 1);
+
+
             });
             services.AddControllers();
         }
